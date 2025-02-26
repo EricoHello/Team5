9dff7418d38b62fd78d6d60ed6f8b5ed77d806c6
Eric Samitov <samitoveric24@gmail.com>
2025-02-26 15:37:09 -0800

    Added security features

--- dbconnect.mjs
+++ dbconnect.mjs
@@ -9,27 +9,27 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 const db = mysql.createConnection({
-    host: "vergil.u.washington.edu",
+    host: "na",
     user: "root",
-    password: "I118whenB210",
+    password: "na",
     database: "learn2code_db",
-    port: 12455
+    port: XXXXX
 });
 
-db.connect(function(err) {
+db.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
 });
 
 app.get('/lessons', (res) => {
-    db.query("SELECT id, title, content, video_url FROM lessons", function(err, result) {
+    db.query("SELECT id, title, content, video_url FROM lessons", function (err, result) {
         if (err) throw err;
         res.send(result);
     })
 });
 
 app.get('/quiz/:lesson_id', (req, res) => {
-    db.query("SELECT id, question, choices, correct_answer FROM quizzes WHERE lesson_id = ?", [req.params.lesson_id], function(err, result) {
+    db.query("SELECT id, question, choices, correct_answer FROM quizzes WHERE lesson_id = ?", [req.params.lesson_id], function (err, result) {
         if (err) throw err;
         res.send(result);
     })
