bc3a8b7a8d8c93d936544714d8723cb931e7ecb0
Matt David <mdavid@cbcbellevue.com>
2025-02-27 18:04:14 -0800

    You can now submit the quiz and the correct answers will be highlighted green

--- app.mjs
+++ app.mjs
@@ -1,9 +1,14 @@
 //app.mjs
 import express from 'express';
+import bodyParser from 'body-parser'
 import { connectToDatabase } from './dbconnect.mjs';
 
 const app = express();
 const port = 1234;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 let pool = await connectToDatabase();
 
@@ -23,6 +28,14 @@ app.get('/quiz', async (req, res) => {
     } catch (error) {
         res.status(500).send('Error retrieving quiz questions');
     }
+});
+
+app.post('/submit-quiz', async (req, res) => {
+    const result = await pool.request().query('SELECT * FROM MultipleChoiceQuestions');
+    const questions = result.recordset;
+    const answers = req.body;
+
+    res.render('submit-quiz', { questions, answers });
 });
 
 app.get('/api/questions', async (req, res) => {
