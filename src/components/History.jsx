285d981e63de6d03be7fd6f1f5db8b4aa3ba1f21
Adonyas <adonigussie94@gmail.com>
2025-02-27 21:13:34 -0800

    vitecode

---
+++ src/components/History.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './style.css';
+
+const History = () => {
+    return (
+        <>
+            <h1> this is the history page</h1>
+        </>
+    )
+};
+
+
+export default History;
+
+
+
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './style.css';
+
+const Home = () => {
+    return (
+        <>
+            <h1> this is the home page</h1>
+        </>
+    )
+};
+
+
+export default Home;
+
+
+
