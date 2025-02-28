1e1761dd1917d5fdbbb0b2fee90f0999320554b1
Matt David <mdavid@cbcbellevue.com>
2025-02-27 22:05:25 -0800

    Added a bunch of questions and made the homepage display multiple quizzes

--- app.mjs
+++ app.mjs
@@ -2,6 +2,7 @@
 import express from 'express';
 import bodyParser from 'body-parser'
 import { connectToDatabase } from './dbconnect.mjs';
+import sql from 'mssql';
 
 const app = express();
 const port = 1234;
@@ -14,27 +15,37 @@ let pool = await connectToDatabase();
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Home', message: 'Welcome to Learn2Code!' });
+app.get('/', async (req, res) => {
+    const result = await pool.request().query('SELECT * FROM Lesson');
+    console.log(result.recordset);
+
+    res.render('index', { title: 'Home', message: 'Welcome to Learn2Code!', lessons: result.recordset });
 });
 
-app.get('/quiz', async (req, res) => {
+app.get('/quiz/:lesson_id', async (req, res) => {
     try {
-        // Query the database for quiz questions
-        const result = await pool.request().query(`SELECT * FROM MultipleChoiceQuestions`);
+        const lessonId = parseInt(req.params.lesson_id, 10);
+        // Query the database for quiz questions using a parameterized query
+        const result = await pool.request()
+            .input('lessonId', sql.Int, lessonId)
+            .query('SELECT * FROM MultipleChoiceQuestions WHERE LessonID = @lessonId');
         console.log(result.recordset);
         // Pass the quiz questions to the view
-        res.render('quiz', { questions: result.recordset });
+        res.render('quiz', { questions: result.recordset, lessonId });
     } catch (error) {
+        console.error('Error retrieving quiz questions', error);
         res.status(500).send('Error retrieving quiz questions');
     }
 });
 
-app.post('/submit-quiz', async (req, res) => {
-    const result = await pool.request().query('SELECT * FROM MultipleChoiceQuestions');
+app.post('/submit-quiz/:lesson_id', async (req, res) => {
+    const lessonId = parseInt(req.params.lesson_id, 10);
+
+    const result = await pool.request()
+    .input('lessonId', sql.Int, lessonId)
+    .query('SELECT * FROM MultipleChoiceQuestions WHERE LessonID = @lessonId');
     const questions = result.recordset;
     const answers = req.body;
-
     res.render('submit-quiz', { questions, answers });
 });
 
