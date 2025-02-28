10bf99d8706257292564a553e8a39257d5284490
Matt David <mdavid@cbcbellevue.com>
2025-02-27 22:57:03 -0800

    vitecode

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import Home from './components/Home';
+import TestMe from './components/TestMe';
+import Projects from './components/Projects';
+import History from './components/History';
+
+
+import './App.css';
+
+
+function App() {
+  const [page, setPage] = useState('home');
+
+  const navigateToPage = (pageName) => {
+    setPage(pageName);
+  };
+
+
+  return (
+    <div>
+      <header>
+        <div id="mainNav">
+          <h1 id="logo">Learn2Code</h1>
+          <button onClick={() => navigateToPage('home')}>Home</button>
+          <button onClick={() => navigateToPage('testme')}>Test Me</button>
+          <button onClick={() => navigateToPage('History')}>History</button>
+          <button onClick={() => navigateToPage('Projects')}>Projects</button>
+        </div>
+      </header>
+
+
+      <div id="container">
+        <div id="mainContent">
+          {page === 'home' && <Home />}
+          {page === 'testme' && <TestMe />}
+          {page === 'History' && <History />}
+          {page === 'Projects' && <Projects />}
+        </div>
+      </div>
+      <footer>
+        &copy; 2025 Learn2Code. All rights reserved.
+      </footer>
+    </div>
+  );
+}
+
+
+export default App;
+
+
+
